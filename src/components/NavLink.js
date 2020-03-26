e088a2de7a384381d335da049a0cb404b07a643b
Kevin <kevinolsson305@gmail.com>
2020-03-26 19:45:09 +0100

    removed stuff

--- src/components/NavLink.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
-
-export default ({ className, children, ...props }) => (
-  <NavLink {...props} className={`NavLink ${className || ''}`}>
-    {children}
-  </NavLink>
-)
